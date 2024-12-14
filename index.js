af14b405b508fd7a04dfb5489ecde3bd53b28b5d
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2024-12-14 15:51:06 +0300

    Update index.css, index.html, and index.js files with minor changes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+// dodger.js
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  module.exports = { moveDodgerLeft, moveDodgerRight };
